15fe46cfbd75f08054a8ead909759810f8570be1
LUCIANO DE LIMA MOURA <roshinomo@gmail.com>
2026-03-11 09:21:29 -0400

    Delete pontuacoes.sql

--- pontuacoes.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE pontuacoes (
-    ->     id INT AUTO_INCREMENT PRIMARY KEY,
-    ->     jogador_id INT,
-    ->     partida_id INT,
-    ->     score_inicial INT DEFAULT 0,
-    ->     score_final INT DEFAULT 0,
-    ->     FOREIGN KEY (jogador_id) REFERENCES jogadores(id),
-    ->     FOREIGN KEY (partida_id) REFERENCES partidas(id)
-    -> );
