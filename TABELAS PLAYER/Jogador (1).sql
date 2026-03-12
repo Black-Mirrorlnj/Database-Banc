3a5c5749216dc892a139021bc5162464c411403b
LUCIANO DE LIMA MOURA <roshinomo@gmail.com>
2026-03-12 10:07:44 -0400

    Delete TABELAS PLAYER directory

--- TABELAS PLAYER/Jogador (1).sql
+++
@@ -1,7 +0,0 @@
-create table jogadores(
-    -> id INT AUTO_INCREMENT PRIMARY KEY,
-    -> steam_id VARCHAR(50) UNIQUE,
-    -> NOME VARCHAR(100),
-    -> criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    -> ultimo_login DATETIME
-    -> );
