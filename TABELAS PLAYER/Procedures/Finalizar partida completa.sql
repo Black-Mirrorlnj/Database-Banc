51ee095f6a187890a1580dea2857844e745120ba
LucianoBR011 <roshinomo@gmail>
2026-03-12 09:43:00 -0400

    Parte do código da tabela de ranking global com dados iniciais. Junto com a store procedure para finalizar partida completa, com atualização de ranking

---
+++ TABELAS PLAYER/Procedures/Finalizar partida completa.sql
@@ -0,0 +1,43 @@
+DELIMITER //
+
+CREATE PROCEDURE finalizar_partida_completa(
+    IN p_jogador_id INT,
+    IN p_partida_id INT,
+    IN p_score_final INT,
+    IN p_kills INT,
+    IN p_deaths INT
+)
+BEGIN
+
+-- Atualiza score final --
+UPDATE pontuacoes
+SET score_final = p_score_final
+WHERE jogador_id = p_jogador_id
+AND partida_id = p_partida_id;
+
+-- Atualiza estatísticas --
+UPDATE estatisticas
+SET 
+    kills = kills + p_kills,
+    deaths = deaths + p_deaths,
+    experiencia = experiencia + p_score_final
+WHERE jogador_id = p_jogador_id;
+
+-- Atualiza nível --
+UPDATE estatisticas
+SET nivel = FLOOR(experiencia / 100) + 1
+WHERE jogador_id = p_jogador_id;
+
+-- Atualiza ranking global --
+UPDATE ranking_global
+SET pontos = pontos + p_score_final
+WHERE jogador_id = p_jogador_id;
+
+END //
+
+DELIMITER ;
+
+
+--Execute do procedimento--
+CALL finalizar_partida_completa(2, 3, 250, 12, 4);
+
