cc5c02fa2fad9b3075373bfab511518461f3b56c
noronham464-design <noronham464@gmail.com>
2026-03-13 09:09:29 -0400

    Create atualizar usuário.sql

---
+++ TABELA USUARIO/Procedures/atualizar usuário.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE PROCEDURE sp_update_user (
+    IN p_userId INT,
+    IN p_username VARCHAR(50),
+    IN p_password VARCHAR(100)
+)
+BEGIN
+    UPDATE users
+    SET username = p_username,
+        password = p_password
+    WHERE userId = p_userId;
+END $$
+
+DELIMITER ;
